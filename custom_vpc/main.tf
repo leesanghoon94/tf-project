1b68c8860f1a33cab6d91406b39e8f714d763abe
leesanghoon94 <leesanghoon94@github.com>
2023-10-29 04:51:33 +0900

    src optimization

--- custom_vpc/main.tf
+++ custom_vpc/main.tf
@@ -4,8 +4,9 @@ resource "aws_vpc" "default" {
     Name = "default_tf_vpc_${var.env}"
   }
 }
+
 resource "aws_subnet" "public_subnet" {
-  count = var.env == "prod" ? 0 : 1
+  count = var.env == "tf-test-prod" ? 0 : 1
   vpc_id     = aws_vpc.default.id
   cidr_block = "10.0.0.0/24"
 
@@ -15,6 +16,7 @@ resource "aws_subnet" "public_subnet" {
     Name = "default_tf_public_subnet_${var.env}"
   }
 }
+
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.default.id
   cidr_block = "10.0.10.0/24"
@@ -25,19 +27,3 @@ resource "aws_subnet" "private_subnet" {
     Name = "default_tf_private_subnet_${var.env}"
   }
 }
-
-/* resource "aws_nat_gateway" "tf_nat_gw" {
-  connectivity_type = "private"
-  subnet_id = aws_subnet.private_subnet.id
-  tags = {
-    Name = "tf_nat_gw_${var.env}"
-  }
-}
-
-
-resource "aws_internet_gateway" "tf_igw" {
-    vpc_id = aws_vpc.default.id
-    tags = {
-      Name = "tf_igw_${var.env}"
-    }
-} */
--- main.tf
+++ main.tf
@@ -6,66 +6,43 @@ terraform {
     }
   }
   cloud {
-    hostname     = "app.terraform.io"
+  
     organization = "tf-sanghoon"
-
+    
     workspaces {
-      name = "tf-test"
+      name = "test-prod" 
     }
   }
-  /* backend "s3" {
-    bucket = "tf-backend-sanghoon"
-    key    = "terraform.tfstate"
-    region = "ap-northeast-2"
-    workspace_key_prefix = "test"
-  } */
 }
+
 provider "aws" {
   region = "ap-northeast-2"
 }
-/* module "default_custom_vpc" {
-  source = "./custom_vpc"
-  env    = "dev"
-}
-module "prd_custom_vpc" {
-  source = "./custom_vpc"
-  env    = "prd"
-} */
-/* variable "envs" {
-  type    = list(string)
-  default = ["dev", "prd", ""]
-} */
+
 module "main_vpc" {
-  /* for_each = toset([for env in var.envs : env if env != ""]) */
+
   source = "./custom_vpc"
   env    = terraform.workspace
-  /* env      = each.key */
+
 }
-/* variable "names" {
-  type    = list(string)
-  default = ["sanghoon", "lee"]
-}
-module "personal_custom_vpc" {
-  for_each = toset([for name in var.env : "${name}_human"])
-  source   = "./custom_vpc"
-  env      = "personal_${each.key}"
-} */
+
 resource "aws_s3_bucket" "tf_backend" {
-  count  = terraform.workspace == "default" ? 1 : 0
+  count = terraform.workspace == "default" ? 1 : 0
   bucket = "tf-backend-sanghoon"
   tags = {
     Name = "tf_backend"
   }
 }
+
 resource "aws_s3_bucket_acl" "tf_backend_acl" {
-  count      = terraform.workspace == "default" ? 1 : 0
+  count = terraform.workspace == "default" ? 1 : 0
   bucket     = aws_s3_bucket.tf_backend[0].id
   acl        = "private"
   depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
-  count  = terraform.workspace == "default" ? 1 : 0
+  count = terraform.workspace == "default" ? 1 : 0
   bucket = aws_s3_bucket.tf_backend[0].id
   rule {
     object_ownership = "ObjectWriter"
@@ -73,20 +50,9 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
 }
 
 resource "aws_s3_bucket_versioning" "tf_backend_versioning" {
-  count  = terraform.workspace == "default" ? 1 : 0
+  count = terraform.workspace == "default" ? 1 : 0
   bucket = aws_s3_bucket.tf_backend[0].id
   versioning_configuration {
     status = "Enabled"
   }
 }
-/* 43.202.127.94 */
-/* 3.39.38.78 */
-/* 43.200.40.236 */
-/* resource "aws_eip" "tf_eip" {
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip}"
-  }
-  tags = {
-    Name = "tf_eip"
-  }
-} */
