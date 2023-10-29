abec5106cf1f790d7feb6851e637e81f21cd0de4
leesanghoon94 <leesanghoon94@github.com>
2023-10-30 00:42:02 +0900

    add elb

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
   cloud {
-  
+
     organization = "tf-sanghoon"
-    
+
     workspaces {
-      name = "test-prod" 
+      name = "test-prod"
     }
   }
 }
@@ -20,7 +20,7 @@ provider "aws" {
 }
 
 module "tf_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "3.18.1"
 
   name = "tf-vpc_${terraform.workspace}"
@@ -29,25 +29,63 @@ module "tf_vpc" {
   azs             = ["ap-northeast-2a", "ap-northeast-2c"]
   private_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
   public_subnets  = ["10.0.100.0/24", "10.0.101.0/24"]
-
+  /* 
   enable_nat_gateway = true
   single_nat_gateway = false
-  one_nat_gateway_per_az = true
+  one_nat_gateway_per_az = true */
   /* enable_vpn_gateway = true */
 
   manage_default_security_group = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = terraform.workspace
   }
 }
 resource "aws_instance" "web_instance" {
-  count = 2
-  ami = "ami-035233c9da2fabf52"
+  count         = 2
+  ami           = "ami-035233c9da2fabf52"
   instance_type = "t2.micro"
-  subnet_id = module.tf_vpc.public_subnets[count.index]
+  subnet_id     = module.tf_vpc.public_subnets[count.index]
   tags = {
     Name = "tf-ec2${count.index}"
   }
+}
+
+module "tf_elb" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "~> 2.0"
+
+  name = "tf-elb"
+
+  subnets         = module.tf_vpc.public_subnets
+  security_groups = [module.tf_vpc.default_security_group_id]
+  internal        = false
+
+  listener = [
+    {
+      instance_port     = 80
+      instance_protocol = "HTTP"
+      lb_port           = 80
+      lb_protocol       = "HTTP"
+    },
+    {
+      instance_port      = 8080
+      instance_protocol  = "http"
+      lb_port            = 8080
+      lb_protocol        = "http"
+      ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
+    },
+  ]
+
+  health_check = {
+    target              = "HTTP:80/"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
+  // ELB attachments
+  number_of_instances = 2
+  instances           = aws_instance.web_instance[*].id
 }
