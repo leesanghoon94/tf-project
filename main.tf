5dc01340890bfa4ad2899840b1c07ebaa4a0cbe4
leesanghoon94 <leesanghoon94@github.com>
2023-10-31 02:40:41 +0900

    modify

--- main.tf
+++ main.tf
@@ -17,17 +17,20 @@ terraform {
 provider "aws" {
   region = "ap-northeast-2"
 }
+
 locals {
   default_vpc_id = "vpc-0bd752928d316c1e4"
-  key_pair_name = "lsh"
-  myip = "122.37.29.17"
+  key_pair_name  = "lsh"
+  myip           = "122.37.29.17"
 }
-resource "aws_instance" "tf-ansible" {
-  ami           = "ami-035233c9da2fabf52"
-  instance_type = "t2.micro"
-  key_name      = local.key_pair_name
 
+resource "aws_instance" "tf-ansible" {
+  ami                    = "ami-035233c9da2fabf52"
+  instance_type          = "t2.micro"
+  key_name               = local.key_pair_name
+  subnet_id              = "subnet-02683abfcb7729c5e"
   vpc_security_group_ids = [aws_security_group.ansible_test_sg.id]
+
   tags = {
     Name = "ansible-test"
   }
